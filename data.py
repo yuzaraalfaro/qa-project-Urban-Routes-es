f4b1654658301a7cf50b50bf6fa1f076c3b7708e
yuzaraalfaro <47545831+yuzaraalfaro@users.noreply.github.com>
2024-05-20 22:40:24 -0500

    Update data.py

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-urban_routes_url = 'https://029f5e89-1ed2-457a-8c61-c3ff8b9e57c2.serverhub.tripleten-services.com'
+urban_routes_url = 'https://cnt-ddfb6151-2368-4dc7-bec4-25d632af12dc.containerhub.tripleten-services.com/'
 address_from = 'East 2nd Street, 601'
 address_to = '1300 1st St'
 phone_number = '+1 123 123 12 12'
